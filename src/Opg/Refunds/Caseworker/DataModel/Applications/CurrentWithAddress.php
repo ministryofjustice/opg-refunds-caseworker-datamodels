e133abbc70dc5d20cb0f8d35211ad8e1a90743a6
David Winchurch <david.winchurch@iterativeit.com>
2017-10-02 11:53:56 +0100

    Missed address on CurrentWithAddress

--- src/Opg/Refunds/Caseworker/DataModel/Applications/CurrentWithAddress.php
+++ src/Opg/Refunds/Caseworker/DataModel/Applications/CurrentWithAddress.php
@@ -12,6 +12,25 @@ class CurrentWithAddress extends Current
     protected $address;
 
     /**
+     * @return Address
+     */
+    public function getAddress(): Address
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param Address $address
+     * @return $this
+     */
+    public function setAddress(Address $address): CurrentWithAddress
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    /**
      * Map properties to correct types
      *
      * @param string $property
